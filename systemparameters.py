e80cad1c2935dadaf5c0a18beb6328091b5bd181
NBH <nbh@netcompany.com>
2018-11-27 08:25:39 +0100

    simplified program

--- systemparameters.py
+++ systemparameters.py
@@ -1,26 +1,23 @@
 import csv
 import os
+import random
+import string
 import uuid
 
 """
 Mandatory fields
 """
-inputFile = 'satser.csv'
-outputFile = 'CopyPasteToFile.sql'
-includesHeaderRow = False  # Skips first row
-includesTypeAndAttribute = False  # uses row to create type and attributes
-overrideToUpdateSql = False
+parameterType = 'haendelsestype'
+parameterTypeId = '53a61d18-f1a8-4b77-933b-5f07cbceajef'
 
 """
-Optional fields, depending on 
-    @param includeTypeAndAttribute 
+Static fields
 """
-parameterType = ''
-parameterTypeId = '03b17275-c3d0-40de-8d73-5638e7cd2509'
-parameterAttributeId = ['49d6f5b9-f2ce-4438-9b62-5cb042374444',
-                        '49d6f5b9-f2ce-4438-9b62-5cb042377410',
-                        '49d6f5b9-f2ce-4438-9b62-5cb042377409',
-                        '49d6f5b9-f2ce-4438-9b62-5cb042377408']
+attributesIdInInputFile = True  # False if Attributes ID are listed in parameterAttributeId
+outputFile = 'SystemparametersOutput.sql'
+parameterAttributeId = []
+systemparametersInput = 'SystemparametersInput.csv'
+
 
 def new_uuid():
     return str(uuid.uuid4())
@@ -30,16 +27,12 @@ def replace(string):
     return string.replace(',', ';').strip()
 
 
-def sql_for_type(type):
-    return 'insert into PARAMETERTYPE (ID,NAVN,OPRETTET,OPRETTETAF,AENDRET,AENDRETAF) values (\'' + parameterTypeId + '\',\'' + type + '\', systimestamp,\'STAMDATA\', systimestamp,\'STAMDATA\');\n'
+def instance_key(size=6, chars=string.ascii_uppercase + string.digits):
+    return parameterType + '-' + ''.join(random.choice(chars) for _ in range(size))
 
 
-def sql_for_attribut(id, datatype, attribute, mandatory, sort, edit, visable):
-    return 'insert into PARAMETERATTRIBUT (ID,PARAMETERTYPE_ID,DATATYPE,NAVN,OBLIGATORISK,SORTERING,IKKE_REDIGERBAR,IKKE_SYNLIG,OPRETTET,OPRETTETAF,AENDRET,AENDRETAF) values (\'' + id + '\',\'' + parameterTypeId + '\',\'' + datatype + '\',\'' + attribute + '\',\'' + mandatory + '\',\'' + sort + '\',\'' + edit + '\',\'' + visable + '\', systimestamp,\'STAMDATA\', systimestamp,\'STAMDATA\');\n'
-
-
-def sql_for_instance(id, noegle):
-    return 'insert into PARAMETERINSTANS (ID,PARAMETERTYPE_ID,NOEGLE,GYLDIG_FRA,OPRETTET,OPRETTETAF,AENDRET,AENDRETAF) values (\'' + id + '\',\'' + parameterTypeId + '\',\'' + noegle + '\',to_date(\'2018-01-01\',\'YYYY-MM-DD\'),systimestamp,\'STAMDATA\',systimestamp,\'STAMDATA\');\n'
+def sql_for_instance(id):
+    return 'insert into PARAMETERINSTANS (ID, PARAMETERTYPE_ID, NOEGLE, GYLDIG_FRA, OPRETTET, OPRETTETAF, AENDRET, AENDRETAF) values (\'' + id + '\',\'' + parameterTypeId + '\',\'' + instance_key() + '\',to_date(\'2018-01-01\',\'YYYY-MM-DD\'),systimestamp,\'STAMDATA\',systimestamp,\'STAMDATA\');\n'
 
 
 def sql_for_value(instance_id, attribut_id, value):
@@ -47,38 +40,11 @@ def sql_for_value(instance_id, attribut_id, value):
         value) + '\',systimestamp, \'STAMDATA\', systimestamp, \'STAMDATA\');\n'
 
 
-def sql_for_update_value(id, value):
-    return 'update PARAMETERVAERDI set VAERDI=\'' + replace(value) + '\' where ID=\'' + id + '\';\n'
-
-
-def create_type_and_attribute(header):
-    type = header[0]
-    with open(header[1], 'r', ):
-        attributes = header[1:]
-    parameter_type_id = new_uuid()
-    for i in attributes:
-        parameterAttributeId.append(new_uuid())
-    predefinition = 'INSERT ALL\nWHEN (c <1) THEN\n'
-    typeString = sql_for_type(parameter_type_id, type)
-
-    attributeStrings = []
-    for i in range(attributes):
-        attributeStrings.append(sql_for_attribut(parameterAttributeId[i], attributes[i]))
-
-    return
-
-
 def create_instance_and_value(line):
     instance_id = new_uuid()
-    data = [sql_for_instance(instance_id, line[0])]
-    for index, value in enumerate(line[1:]):
-        data.append(sql_for_value(instance_id, parameterAttributeId[index-1], value))
-
-    write_file(outputFile, data)
-
-
-def update_values(line):
-    data = [sql_for_update_value(line[0], line[1])]
+    data = [sql_for_instance(instance_id)]
+    for index, value in enumerate(line):
+        data.append(sql_for_value(instance_id, parameterAttributeId[index], value))
     write_file(outputFile, data)
 
 
@@ -122,21 +88,15 @@ if __name__ == '__main__':
     """
     Read input file content
     """
-    data = read_file(inputFile)
+    data = read_file(systemparametersInput)
     """
-    First line defines the Type and attributes
+    First line defines the Attributes
     """
-    if includesHeaderRow:
-        data.pop(0)
-    if includesTypeAndAttribute:
-        header = data.pop(0)
-        create_type_and_attribute(header)
+    if attributesIdInInputFile:
+        parameterAttributeId = data.pop(0)
+
     """
     Rest defines the values for each attribute
     """
-    if overrideToUpdateSql:
-        for line in data:
-            update_values(line)
-    else:
-        for line in data:
-            create_instance_and_value(line)
+    for line in data:
+        create_instance_and_value(line)
--- tekstnoegler.py
+++ tekstnoegler.py
@@ -1,17 +1,31 @@
 import csv
+import os
+
+inputFile = 'tekstnogler.csv'
+outputFile = 'tekster.sql'
+
 
 def opretTekstnoegle(tekstnogle, tekst):
     query = 'INSERT INTO TEKST (ID,SPROG,TEKSTNOEGLE,TEKST,HJAELPETEKST,BESKRIVELSETEKST,FOERTEKST,EFTERTEKST,PLACEHOLDERTEKST,GENVEJSTAST,OPRETTET,OPRETTETAF,AENDRET,AENDRETAF) ' \
             'SELECT sys_guid(),\'da\',\'' + tekstnogle + '\',\'' + tekst + '\',\'\',\'\',\'\',\'\',\'\',\'\',systimestamp,\'SYSTEM\',systimestamp,\'SYSTEM\' ' \
-            'FROM dual WHERE NOT EXISTS (SELECT 1 FROM TEKST WHERE SPROG = \'da\' AND TEKSTNOEGLE = \'' + tekstnogle + '\')\n/\n'
-    update = 'UPDATE TEKST SET TEKST=\'' + tekst + '\' WHERE TEKSTNOEGLE=\'' + tekstnogle + '\' AND dbms_lob.compare(TEKST, to_clob(TEKSTNOEGLE)) = 0\n/\n'
+                                                                           'FROM dual WHERE NOT EXISTS (SELECT 1 FROM TEKST WHERE SPROG = \'da\' AND TEKSTNOEGLE = \'' + tekstnogle + '\');\n'
+    update = 'UPDATE TEKST SET TEKST=\'' + tekst + '\' WHERE TEKSTNOEGLE=\'' + tekstnogle + '\' AND dbms_lob.compare(TEKST, to_clob(TEKSTNOEGLE)) = 0;\n'
 
-    with open('tekster.sql', 'a') as file:
+    with open(outputFile, 'a') as file:
         file.write(query)
         file.write(update)
 
+
+def prep_new_parameters():
+    try:
+        os.remove(outputFile)
+    except OSError:
+        pass
+
+
 if __name__ == "__main__":
-    with open('tekstnogler.csv', 'r', newline='') as csvFile:
+    with open(inputFile, 'r', newline='') as csvFile:
+        prep_new_parameters()
         reader = csv.reader(csvFile, delimiter=';', quotechar='|')
         for row in reader:
             opretTekstnoegle(row[0], row[1])
